cd69640e91fe2347141210d0f60ae3222d5a912e
Mohammad Kiani <kianimohammad@outlook.com>
2020-01-10 17:41:36 -0500

    User Location Demo

--- Location Demo/ViewController.swift
+++ Location Demo/ViewController.swift
@@ -7,12 +7,90 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    // create a variable of CLLocationManager
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // show user location on map view
+//        mapView.showsUserLocation = true
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // grabbing the user location
+        let userLocation: CLLocation = locations[0]
+        
+        let lat = userLocation.coordinate.latitude
+        let long = userLocation.coordinate.longitude
+        
+        let latDelta: CLLocationDegrees = 0.05
+        let longDelta: CLLocationDegrees = 0.05
+        
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        let location = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+//        print(userLocation)
+        
+        // finding the user address from the user location
+        CLGeocoder().reverseGeocodeLocation(userLocation) { (placemarks, error) in
+            if let error = error {
+                print(error)
+            } else {
+                if let placemark = placemarks?[0] {
+                    
+                    var subThoroufare = ""
+                    if placemark.subThoroughfare != nil {
+                        subThoroufare = placemark.subThoroughfare!
+                    }
+                    
+                    var thoroufare = ""
+                    if placemark.thoroughfare != nil {
+                        thoroufare = placemark.thoroughfare!
+                    }
+                    
+                    var subLocality = ""
+                    if placemark.subLocality != nil {
+                        subLocality = placemark.subLocality!
+                    }
+                    
+                    var subAdministrativeArea = ""
+                    if placemark.subAdministrativeArea != nil {
+                        subAdministrativeArea = placemark.subAdministrativeArea!
+                    }
+                    
+                    var postalCode = ""
+                    if placemark.postalCode != nil {
+                        postalCode = placemark.postalCode!
+                    }
+                    
+                    var country = ""
+                    if placemark.country != nil {
+                        country = placemark.country!
+                    }
+                    
+                    print(subThoroufare + thoroufare + "\n" + subLocality + "\n" +
+                            subAdministrativeArea + "\n" +
+                            postalCode + "\n" + country)
+                }
+            }
+        }
     }
 
 
